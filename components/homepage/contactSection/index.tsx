8c2abf066e48f25b17be3608a93d970ebd9f66f5
priest-2105 <priest2105@gmail.com>
2025-01-23 03:32:04 +0100

    deployment

--- components/homepage/contactSection/index.tsx
+++ components/homepage/contactSection/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import Link from "next/link"
+
 import { Button } from "@/components/ui/button"
 import { HandshakeIcon } from "lucide-react"
 
--- components/homepage/heroSection/index.tsx
+++ components/homepage/heroSection/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import NavbarSection from "@/components/layout/navbar"
